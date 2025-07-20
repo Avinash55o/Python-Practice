b74f79904cc04eabb98f1552d0b26cb7dc044120
Avinash Boruah <avinashboruah8@gmail.com>
2025-07-20 10:32:57 +0530

    Add initial implementation of list operations and index retrieval

---
+++ 9_list_problem.py
@@ -0,0 +1,16 @@
+list=[1,2,3,"5"]
+list.insert(-1,4) # to insert an item at last
+list.insert(0,0)
+
+list.append(6) # put the 6 at last
+
+if list[3] == "5":
+    print("hello")
+
+
+print(list)
+
+# to see the index
+c=list.index(1)
+print(c)
+
