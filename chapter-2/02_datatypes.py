7761f4fa8c64f16138b471248b1e915bbc64cd03
Avinash Boruah <avinashboruah8@gmail.com>
2025-08-12 14:48:44 +0530

    chapter-2

---
+++ chapter-2/02_datatypes.py
@@ -0,0 +1,7 @@
+# Data types in Python
+
+a= 10 # it stands for integer
+b= 10.5 # it stands for float
+c= "Hello" # it stands for string
+d= True # it stands for boolean
+e=None # it stands for NoneType
---
+++ chapter-2/03_input.py
@@ -0,0 +1,5 @@
+a_=input("Enter a number: ")
+print(type(a_)) # This will give str type
+
+# Input in Python
+a=int(input("Enter a number: "))
