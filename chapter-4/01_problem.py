c3559cfdd2384dedb54ce9156861f3b5c1c1c604
Avinash Boruah <avinashboruah8@gmail.com>
2025-08-13 21:06:52 +0530

    list

---
+++ chapter-4/01_problem.py
@@ -0,0 +1,7 @@
+# store fruit names in list that user gives
+fruits = []
+for i in range(7):
+    fruit=input("enter the name of fruit: ")
+    fruits.append(fruit)
+print(fruits)
+print(len(fruits))  # prints the length of the list
---
+++ chapter-4/02_problem.py
@@ -0,0 +1,5 @@
+# to arrange the list in sorted order
+fruits=["apple", "orange", "kiwi", "tomato", "banana", "grape", "mango"]
+
+fruits.sort()  # sorts the list in ascending order
+print(fruits)  # prints the sorted list
