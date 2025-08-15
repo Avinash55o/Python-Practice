4e18ca383f6e8f3d4a20d8234f3312a54c59291b
Avinash Boruah <avinashboruah8@gmail.com>
2025-08-15 23:43:29 +0530

    factorial by while loop

--- chapter-6/03_factorial.py
+++ chapter-6/03_factorial.py
@@ -1,8 +1,19 @@
 # 5!= 1 x 2 x 3 x 4 x 5
 
 n=int(input("enter the no: "))
+
+# WITH FOR LOOP
 product=1
 for i in range(1,n+1):
     product= product *i
 
 print(f"the factorial of {n}:",product);
+
+
+# WITH WHILE LOOP
+i=1
+product_1=1
+while i<=n:
+    product_1=product_1 * i
+    i += 1;
+print(f"the factoial of {n}",product_1)
