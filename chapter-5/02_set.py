d7929cc62a8acb819cfc5a5dcce54d46a8c4b284
Avinash Boruah <avinashboruah8@gmail.com>
2025-08-15 23:17:29 +0530

    set

---
+++ chapter-5/02_set.py
@@ -0,0 +1,13 @@
+# empty set 
+s=set()
+
+s={1, 2, 3, 4, 5}
+
+# set with mixed data types
+s={1, 2.5, 'hello', (1, 2)}
+
+# set dosn't allow duplicates
+# methods
+
+s.add(6)  # adds an element to the set
+s.remove(3) #remove the element
