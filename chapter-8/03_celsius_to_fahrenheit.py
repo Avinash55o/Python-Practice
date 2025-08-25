cb2fba7d1a2de46c3091be27f728d974dbc179cb
Avinash Boruah <avinashboruah8@gmail.com>
2025-08-25 23:38:27 +0530

    celsius to fahrenheit

---
+++ chapter-8/03_celsius_to_fahrenheit.py
@@ -0,0 +1,7 @@
+# celsius to fahrenheit 
+
+def convert_to_fahrenheit(celsius):
+    
+    fahrenheit= celsius * 1.8 + 32
+    print(fahrenheit)
+convert_to_fahrenheit(int(input("enter the temp in celsius")))
